22c082127f4fca3c301e13957d3d3a32d32ae4a6
Jakub Mišina <jakub.misina@gmail.com>
2018-09-26 08:00:39 +0200

    simple command line program added

---
+++ src/main/java/cz/misina/Main.java
@@ -0,0 +1,10 @@
+package cz.misina;
+
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Now it's " + new Date());
+    }
+}
